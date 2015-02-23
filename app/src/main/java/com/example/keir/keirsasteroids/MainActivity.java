548e0a7a511fe1ff3eac488e380704e8dcdd22f8
Keir Sweeney <k1214635@kingston.ac.uk>
2015-02-23 18:16:54 +0000

    Added buttons and did the first part.

--- app/src/main/java/com/example/keir/keirsasteroids/MainActivity.java
+++ app/src/main/java/com/example/keir/keirsasteroids/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.keir.keirsasteroids;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,28 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton start = (ImageButton)findViewById(R.id.btnStart);
+        ImageButton exit = (ImageButton)findViewById(R.id.btnExit);
+        start.getBackground().setAlpha(0);
+        start.setHapticFeedbackEnabled(true);
+        exit.getBackground().setAlpha(0);
+        exit.setHapticFeedbackEnabled(true);
+        start.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent game = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(game);
+            }
+        });
+        exit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int pid= android.os.Process.myPid();
+                android.os.Process.killProcess(pid);
+            }
+        });
+
     }
 
 
