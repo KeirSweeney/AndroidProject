5c05c6f263c9b343c99b11c4300a90c91706a528
Keir Sweeney <k1214635@kingston.ac.uk>
2015-02-23 19:07:05 +0000

    More OpenGL stuff.

--- app/src/main/java/com/example/keir/keirsasteroids/GameObject.java
+++ app/src/main/java/com/example/keir/keirsasteroids/GameObject.java
@@ -75,6 +75,7 @@ public class GameObject {
         gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_REPEAT);
         gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_REPEAT);
         GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
+
         bitmap.recycle();
     }
 }
--- app/src/main/java/com/example/keir/keirsasteroids/GameRenderer.java
+++ app/src/main/java/com/example/keir/keirsasteroids/GameRenderer.java
@@ -12,7 +12,31 @@ import javax.microedition.khronos.opengles.GL10;
  */
 public class GameRenderer implements Renderer {
 
-    //private GameObject background = new GameObject();
+    private GameObject background = new GameObject();
+    private float bgScroll = 0f;
+
+    private float vertices[] = {
+            0.0f, 0.0f, 0.0f,
+            1.0f, 0.0f, 0.0f,
+            1.0f, 1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+    };
+    private float normals[] = {
+            0.0f, -1.0f, 0.0f,
+            0.0f, -1.0f, 0.0f,
+            0.0f, -1.0f, 0.0f,
+            0.0f, -1.0f, 0.0f,
+    };
+    private float textures[] = {
+            0.0f, 0.0f,
+            1.0f, 0f,
+            1f, 1.0f,
+            0f, 1f,
+    };
+    private short faces[] = {
+            0,1,2,
+            0,2,3,
+    };
 
     public GameRenderer() {
         super();
@@ -20,16 +44,53 @@ public class GameRenderer implements Renderer {
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        gl.glEnable(GL10.GL_DEPTH_TEST); // depth test necessary for most 3D scenes
+        gl.glEnable(GL10.GL_NORMALIZE); // necessary for some model formats
+        gl.glShadeModel(GL10.GL_SMOOTH); // smooth shading mode is the default one
+        gl.glEnable(GL10.GL_TEXTURE_2D); // enable texturing
+        gl.glClearDepthf(1.0f); // clear depth buffer
+        // setup lighting
+        gl.glEnable(GL10.GL_LIGHTING);
+        gl.glEnable(GL10.GL_LIGHT0);
+        gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_POSITION,
+                new float [] { 1.0f, 1.0f, 2.5f, 0.0f }, 0);
+        // load the background mesh & texture
+        background.loadTexture(gl, R.drawable.backgroundstars);
+        background.loadMesh(vertices, normals, textures, faces);
+        // All other game objects will be initialised here
+
 
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
+        gl.glViewport(0, 0, width,height);
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+        gl.glOrthof(0f, 1f, 0f, 1f, -10f, 10f);
 
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
+        // limit frame rate to 60 fps
+        try {
+            Thread.sleep(60);
+        } catch (InterruptedException e) { }
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
+        gl.glTranslatef(0.0f, 0.0f, -5.0f);gl.glMatrixMode(GL10.GL_TEXTURE);
+        gl.glLoadIdentity();
+        gl.glTranslatef(0.0f, bgScroll, 0.0f);
+        bgScroll += .002f;
+        if (bgScroll == Float.MAX_VALUE)
+            bgScroll = 0f;
+        gl.glDisable(GL10.GL_LIGHTING);
+        background.draw(gl);
+        gl.glEnable(GL10.GL_LIGHTING);
+
+//All other game drawing will be called here
 
     }
 }
